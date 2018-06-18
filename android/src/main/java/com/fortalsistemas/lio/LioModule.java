cafb0996b94a8c0d910780c1bb0868255c233b1e
Rafael Alves <rafaelalvesufc@gmail.com>
2018-06-18 17:28:14 -0300

    Adição de checkoutOrder

--- android/src/main/java/com/fortalsistemas/lio/LioModule.java
+++ android/src/main/java/com/fortalsistemas/lio/LioModule.java
@@ -9,20 +9,30 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import android.util.Log;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import android.support.annotation.Nullable;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import cielo.orders.domain.Credentials;
 import cielo.orders.domain.Order;
+import cielo.orders.domain.product.PrimaryProduct;
+import cielo.orders.domain.product.SecondaryProduct;
 import cielo.sdk.order.OrderManager;
 import cielo.sdk.order.ServiceBindListener;
+import cielo.sdk.order.payment.PaymentCode;
+import cielo.sdk.order.payment.PaymentError;
+import cielo.sdk.order.payment.PaymentListener;
 
 public class LioModule extends ReactContextBaseJavaModule {
   private String clientID = "";
@@ -30,6 +40,7 @@ public class LioModule extends ReactContextBaseJavaModule {
   private Credentials credentials;
   private OrderManager orderManager;
   private Order order;
+  private PaymentListener paymentListener;
 
   public LioModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -67,7 +78,7 @@ public class LioModule extends ReactContextBaseJavaModule {
       public void onServiceBound() {
         //Você deve garantir que sua aplicação se conectou com a LIO a partir desse listener
         //A partir desse momento você pode utilizar as funções do OrderManager, caso contrário uma exceção será lançada.
-        Log.d("DEBUG", "Serviço conectado com Sucesso");
+        Log.d("DEBUG", "Serviço CIELO conectado com Sucesso");
       }
 
       @Override
@@ -78,6 +89,42 @@ public class LioModule extends ReactContextBaseJavaModule {
     };
 
     orderManager.bind(getCurrentActivity(), serviceBindListener);
+
+    paymentListener = new PaymentListener() {
+      @Override
+      public void onStart() {
+        WritableMap params = Arguments.createMap();
+        params.putString("status", "O pagamento começou.");
+        params.putInt("code", 0);
+        sendEvent(getReactApplicationContext(), "LioOnPayment", params);
+        Log.d("DEBUG", "O pagamento começou.");
+      }
+
+      @Override
+      public void onPayment(@NotNull Order order) {
+        Log.d("DEBUG", "Um pagamento foi realizado.");
+        WritableMap params = Arguments.createMap();
+        params.putString("status", "Um pagamento foi realizado.");
+        params.putInt("code", 1);
+        sendEvent(getReactApplicationContext(), "LioOnPayment", params);
+      }
+
+      @Override public void onCancel() {
+        Log.d("DEBUG", "A operação foi cancelada.");
+        WritableMap params = Arguments.createMap();
+        params.putString("status", "A operação foi cancelada.");
+        params.putInt("code", 2);
+        sendEvent(getReactApplicationContext(), "LioOnPayment", params);
+      }
+
+      @Override public void onError(@NotNull PaymentError paymentError) {
+        Log.d("DEBUG", "Houve um erro no pagamento.");
+        WritableMap params = Arguments.createMap();
+        params.putString("status", "Houve um erro no pagamento.");
+        params.putInt("code", 3);
+        sendEvent(getReactApplicationContext(), "LioOnPayment", params);
+      }
+    };
   }
 
   @ReactMethod
@@ -87,17 +134,38 @@ public class LioModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void addItems(String items){
-    Log.d("DEBUG", items);
-    /*try {
-      JSONObject jsonItems = new JSONObject(items);
+    try {
+      JSONArray jsonItems = new JSONArray(items);
       for(int i = 0; i < jsonItems.length(); i++){
-
+        JSONObject jsonItem = jsonItems.getJSONObject(i);
+        Log.d("DEBUG", jsonItem.getString("descricao"));
+        order.addItem(jsonItem.getString("id_produto"), jsonItem.getString("descricao"), jsonItem.getLong("preco"), jsonItem.getInt("quantidade"), "unidade");
       }
+      orderManager.placeOrder(order);
     } catch (JSONException e) {
       e.printStackTrace();
-    }*/
+    }
+  }
+
+  @ReactMethod
+  public void placeOrder(){
+    orderManager.placeOrder(order);
+  }
+
+  @ReactMethod
+  public void checkoutOrder(int value, String paymentCode){
+    if(paymentCode.equals("debito")){
+      Log.d("DEBUG", paymentCode + " " + value);
+      orderManager.checkoutOrder(order.getId(), 5000, PaymentCode.DEBITO_AVISTA, paymentListener);
+    }
+    else{
+      if(paymentCode.equals("credito")){
+        orderManager.checkoutOrder(order.getId(), 5000, PaymentCode.DEBITO_AVISTA, paymentListener);
+      }
+    }
 
   }
+
   private void sendEvent(ReactContext reactContext,
                          String eventName,
                          @Nullable WritableMap params) {
--- index.android.js
+++ index.android.js
@@ -4,7 +4,8 @@ import { NativeModules, NativeEventEmitter } from 'react-native'
 const EventEmitter = new NativeEventEmitter(NativeModules.Lio || {});
 
 export const LioEvent = {
-    LioServiceErrorReceived: 'LioServiceErrorReceived'
+    LioServiceErrorReceived: 'LioServiceErrorReceived',
+    LioOnPayment: 'LioOnPayment'
 };
 
 const Lio = {};
@@ -15,6 +16,18 @@ Lio.initializeLio = (clientID, accessToken) => {
 
 Lio.createDraftOrder = (orderId) => {
     return NativeModules.Lio.createDraftOrder(orderId);
+}
+
+Lio.addItems = (items) => {
+    return NativeModules.Lio.addItems(items);
+}
+
+Lio.placeOrder = () => {
+    return NativeModules.Lio.placeOrder();
+}
+
+Lio.checkoutOrder = (value, paymentCode) => {
+    return NativeModules.Lio.checkoutOrder(value, paymentCode);
 }
 
 Lio.on = (event, callback) => {
